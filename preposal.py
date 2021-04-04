9552f31d8b192491ebf9d01ef858efa354bf69ba
sabrinahut <80432773+sabrinahut@users.noreply.github.com>
2021-04-04 18:05:05 -0400

    Add files via upload

---
+++ preposal.py
@@ -0,0 +1,15 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+I would like to make some kind of game. The three ideas I have currently are my own spin on battle ship or pacman. I also have a idea of a program that would have you type for a certain amount and from there it can calculate how fast you type and rate you. 
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+At the moment I dont have plants to collaborate with a classmate.
+
+## Do you have any questions of your own?
+
+No, currently I do not have any questions. 
+
+
