bea9ddcaa3149e43b91563d08125460241d9436d
sabrinahut <80432773+sabrinahut@users.noreply.github.com>
2021-04-17 14:29:55 -0400

    Add files via upload

---
+++ final project propsal .py
@@ -0,0 +1,48 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Pythman (python and pacman)
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+A game that serves like pacman but I will make it my own. 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+My software will conduct a game that the user can play. The features will be a pacman style game. The player will play the game like a tradinatial pacman game where they have
+to hide from the predators while trying to win the game. It will be excuated through python. 
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051,
+###and which aspect(s) would relate to the other course?
+
+TODO, if applicable
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+TODO, if applicable
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+A good outcome for my project is that it effectivly works. No matter what I willl accomplish making a pacman game. 
+
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+A better outcome for my project is that people who play my game will enjoy it and want to play it again. I think that I can accomplish a game that not only works but one that
+others will enjoy playing. 
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+The best outcome for my project is that I can figure out a way to redesign the pacman board and make my own with pictures and colors. 
+
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates,
+###who will do what?
+
+My next steps are I am going to look at some code of other games so I can fully understand what type of style and format I need to code. So that I can efficently code the game
+in a way that is orgainzed and formatted in a orderly fashion. The skills I need to accquire is becoming more comfortable with coding long lines of text (code) so I dont
+get over whellmed. I am going to research different ways to implment my game and how to create my icons and character to put into the game. I want to create a pacman style
+game not the same thing as pacman that would not be fun or orginal in the slightest. 
