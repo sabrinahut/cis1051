91e7f13c0e9e1e96d90d7c342aee17390e692249
sabrinahut <80432773+sabrinahut@users.noreply.github.com>
2021-04-25 18:00:11 -0400

    Add files via upload

---
+++ progress report .py
@@ -0,0 +1,30 @@
+# Status Report
+
+#### Your name
+
+Sabrina Huttie 
+
+#### Your section leader's name
+Joanne Nichols
+
+
+#### Project title
+
+Pythman 
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+
+I have created the main basis for it I have the pacman figure created and I have the background. 
+
+#### What have you not done for your project yet?
+
+I still have to create the ghosts. I plan on not using ghost figures and making something else like a turtle from python or something similar. I am having difficulty creating
+the ghosts (turtles) it is more complicated than I thought with the coordinates and such. 
+
+#### What problems, if any, have you encountered?
+
+I am having problems with creating the correct coordinates so the game can be played efficently. 
